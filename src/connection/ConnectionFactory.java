cd33ce2635dbd9875ce903ceeda468b5d8cd779e
Mateus Dal Forno <mateush.dalforno@gmail.com>
2020-11-11 09:06:10 -0300

    Criação da classe de conexão

---
+++ src/connection/ConnectionFactory.java
@@ -0,0 +1,57 @@
+package connection;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class ConnectionFactory {
+
+	private static final String DRIVER = "con.mysql.jdbc.Driver";
+	private static final String URL = "jdbc:mysql://localhost:3306/cinema";
+	private static final String USER = "root";
+	private static final String PASS = "";
+
+	public static Connection getConnection() {
+		try {
+			Class.forName(DRIVER);
+			return DriverManager.getConnection(URL, USER, PASS);
+		} catch (ClassNotFoundException | SQLException e) {
+			throw new RuntimeException("Erro na conexão: ", e);
+		}
+	}
+
+	public static void closeConnection(Connection con) {
+		if (con != null) {
+			try {
+				con.close();
+			} catch (SQLException e) {
+				throw new RuntimeException("Erro ao encerrar a conexão: ", e);
+			}
+		}
+	}
+
+	public static void closeConnection(Connection con, PreparedStatement stmt) {
+		closeConnection(con);
+		if (stmt != null) {
+			try {
+				stmt.close();
+			} catch (SQLException e) {
+				throw new RuntimeException("Erro ao encerrar a conexão: ", e);
+			}
+		}
+	}
+
+	public static void closeConnection(Connection con, PreparedStatement stmt, ResultSet rs) {
+		closeConnection(con, stmt);
+		if (rs != null) {
+			try {
+				rs.close();
+			} catch (SQLException e) {
+				throw new RuntimeException("Erro ao encerrar a conexão: ", e);
+			}
+		}
+	}
+
+}
